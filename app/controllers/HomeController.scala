f9ec262d30eab0a2e269c1ede5897109a183d381
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-02-22 21:31:18 +0100

    Reformated Code

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 /**
@@ -11,14 +10,14 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+    /**
+     * Create an Action to render an HTML page.
+     *
+     * The configuration in the `routes` file means that this method
+     * will be called when the application receives a `GET` request with
+     * a path of `/`.
+     */
+    def index() = Action { implicit request: Request[AnyContent] =>
+        Ok(views.html.index())
+    }
 }
