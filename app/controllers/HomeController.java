b2043da99e5a1af7927d419802fa085bc18c8329
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-02-23 16:55:53 +0100

    First Websocket Implementation

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,14 +1,36 @@
 package controllers;
 
-import de.knxamk.util.factories.StageFactory;
-import play.mvc.*;
+import actors.MyWebSocketActor;
+import akka.actor.ActorSystem;
+import akka.stream.Materializer;
 import de.knxamk.controller.BackendController;
+import de.knxamk.util.factories.StageFactory;
+import play.libs.streams.ActorFlow;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.WebSocket;
+
+import javax.inject.Inject;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+    private final ActorSystem actorSystem;
+    private final Materializer materializer;
+
+    @Inject
+    public HomeController(ActorSystem actorSystem, Materializer materializer) {
+        this.actorSystem = actorSystem;
+        this.materializer = materializer;
+    }
+
+    public WebSocket socket() {
+        return WebSocket.Text.accept(
+                request -> ActorFlow.actorRef(MyWebSocketActor::props, actorSystem, materializer));
+    }
 
     BackendController controller = new BackendController(new StageFactory().createTestStages()[1]);
 
@@ -22,5 +44,7 @@ public class HomeController extends Controller {
     public Result index() {
         return ok(views.html.index.render(controller.toJson()));
     }
+
+
 
 }
